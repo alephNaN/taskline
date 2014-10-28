a196072dd5b3a588f4a0b6d390819ce12f66bcc2
tuanvo <tuanvo@ougl-23-d.eplt.washington.edu>
2014-10-28 14:54:37 -0700

    Set start date. Refresh ADT. CSS Style / Pure UI / Interface improvements

--- code.js
+++ code.js
@@ -1,154 +1,16 @@
 $(document).ready(function() {
-	var startDate = new Date(2014, 9, 20, 6, 0, 0, 0);
+	var startDate = new Date(2014, 9, 23, 6, 0, 0, 0);
 	var currDate = new Date();
-	loadStartDate();
-	loadCurrDate();
-	loadMinutesSince();
 
-	
-	// object for scheduling
-	var adt = (function() {
-		self = this;
-		self.isConstructed = false;
+	var hideDatepickerTimer = null;
+	var newTaskTimer = null;
 
-		function construct(startDate, currDate) {
-			if (self.isConstructed) {
-				throw "Only call constructor once, pretty please";
-			}
-			self.isConstructed = true;
-
-			// begin construction
-			self.startDate = startDate;
-			self.currDate = currDate;
-			self.timeDiff = Math.floor((self.currDate - self.startDate) / 60000);
-			self.tasks = [];
-			self.index = -1;
-
-		}
-
-		function addTask(task) {
-			self.tasks.push(task);
-		}
-
-		function findIndex() {
-			var totalTime = 0;
-			var i = 0;
-			//console.log("self.tasks.length " + self.tasks.length);
-			while ( i < self.tasks.length && totalTime < self.timeDiff) {
-				//console.log("i: " + i);
-				//console.log("totalTime: " + totalTime);
-				var task = $(tasks[i]);
-				var duration = parseInt(task.children(".duration").html());
-				totalTime += duration;
-				i++;
-
-				task.addClass("expectedComplete");
-			}
-		}
-
-		function print() {
-			console.log(self.tasks);
-		}
-
-		function updateTasks() {
-			var containerSelector = "tasks";
-			var container = $("#" + containerSelector);
-			var tasks = container.children();
-			self.tasks = tasks;
-		}
-
-		return {
-			construct: construct,
-			print: print,
-			addTask: addTask,
-			findIndex: findIndex,
-			updateTasks: updateTasks
-		}
-	})();
-	adt.construct(startDate, currDate);
-	/* for (var i = 0 ; i < 50; i++) {
-		var taskDuration = Math.floor(Math.random() * 30) + 15;
-		adt.addTask(taskDuration);
-	} */
-	//adt.print();
-	adt.updateTasks();
-	adt.findIndex();
-
-	function writeDate(date, id) {
-		var year = date.getFullYear();
-		var month = date.getMonth() + 1;
-		var day = date.getDate();
-		var hours = date.getHours();
-
-		var s = month + " / " + day + " / " + year + ", " + hours + " : 00";
-		$("#" + id).append(s);
-	}
-
-	function loadStartDate() {
-		var START_DATE_ID = "start_date";
-
-		writeDate(startDate, START_DATE_ID);
-	}
-
-	function loadCurrDate() {
-		var CURR_DATE_ID = "curr_date";
-
-		writeDate(currDate, CURR_DATE_ID);
-	}
-
-	function loadMinutesSince() {
-		var msInMinute = 60 * 1000;
-		var minutesInHour = 60;
-		var timeDiff = currDate - startDate;
-		var minuteDiff = timeDiff / msInMinute;
-		// round to 2 decimals
-		minuteDiff = Math.round(minuteDiff * 100) / 100;
-		var hourDiff = minuteDiff / minutesInHour;
-		// round to 2 decimals
-		hourDiff = Math.round(hourDiff * 100) / 100;
-
-		var s = minuteDiff + " minutes or " + hourDiff + " hours";
-
-		var id = "time_diff";
-		$("#" + id).append(s);
-	}
-
-	var ENTRY_FORM_ID = "entry_form";
-	var ENTRY_SUBMIT_ID = "entry_submit";
-
-	var ADD_TASK_BUTTON_ID = "add_task";
-
-	var TASK_LIST_ID = "tasks";
-
-	$("#" + ADD_TASK_BUTTON_ID).click(function() {
-		showEntryBox();
-	});
-
-	$("#" + ENTRY_SUBMIT_ID).click(function(e) {
-		e.preventDefault();
-
-		var entry_title_selector = "#" + ENTRY_FORM_ID + " [name=entry_title]";
-		var entry_duration_selector = "#" + ENTRY_FORM_ID + " [name=entry_duration]";
-		var entry_color_selector = "#" + ENTRY_FORM_ID + " select";
-
-		var entryTitle = $(entry_title_selector).val();
-		var entryDuration = $(entry_duration_selector).val();
-		var entryColor = $(entry_color_selector).val();
-
-		addTask(entryTitle, entryDuration, entryColor);
-	});
-
-	function showEntryBox() {
-		$("#" + ENTRY_FORM_ID).show();
-	}
-
-	function addTask(title, duration, color) {
-		var node = "<div class=\"task\">" + title + "<span class=\"duration\">" + duration +"</span> </div>";
-		node = $(node);
-		node.addClass(color);
-		node.addClass("notcomplete");
-		$("#" + TASK_LIST_ID).append(node);
-	}
+	$("#startdate").hide();
+	$("#show_setstartdate").click(function() {
+		clearTimeout(hideDatepickerTimer);
+		$("#startdate").show();
+	})
+	$( "#datepicker" ).datepicker();
 
 	$("#generate_random_shit").click(function(e){
 		e.preventDefault();
@@ -173,6 +35,221 @@ $(document).ready(function() {
 		var entry_color_selector = "#" + ENTRY_FORM_ID + " select";
 		$(entry_color_selector).val(color);
 	});
+	// object for scheduling
+	var adt = (function() {
+		self = this;
+		self.isConstructed = false;
+
+		function construct(startDate, currDate) {
+			if (self.isConstructed) {
+				throw "Only call constructor once, pretty please";
+			}
+			self.isConstructed = true;
+
+			var st
+			self.startDate = startDate;
+			self.currDate = currDate;
+			// begin construction
+			computeTimeDiff();
+			self.tasks = [];
+			self.index = -1;
+
+		}
+
+		function computeTimeDiff() {
+			loadStartDate();
+			loadCurrDate();
+			loadMinutesSince();
+
+			self.timeDiff = Math.floor((self.currDate - self.startDate) / 60000);
+			console.log("self.timeDiff = " + self.timeDiff);
+		}
+		function addTask(task) {
+			self.tasks.push(task);
+		}
+
+		function findIndex() {
+			self.tasks.removeClass("expectedComplete");
+			var totalTime = 0;
+			var i = 0;
+			//console.log("self.tasks.length " + self.tasks.length);
+			while ( i < self.tasks.length && totalTime < self.timeDiff) {
+				//console.log("i: " + i);
+				//console.log("totalTime: " + totalTime);
+				var task = $(self.tasks[i]);
+				var duration = parseInt(task.children(".duration").html());
+				totalTime += duration;
+				console.log("totalTime" + totalTime);
+				i++;
+
+				task.addClass("expectedComplete");
+			} 
+			console.log(i);
+		}
+
+		function print() {
+			console.log(self.tasks);
+		}
+
+		function updateTasks() {
+			var containerSelector = "tasks";
+			var container = $("#" + containerSelector);
+			var tasks = container.children();
+			self.tasks = tasks;
+		}
+
+		// date: string in the form mm/dd/yyyy
+		function setDate(date) {
+			date = date.split("/");
+			var month = date[0] - 1;
+			var day = date[1];
+			var year = date[2];
+
+			console.log("setting date to " + month + ", " + day + " , " + year);
+			date = new Date(year, month, day, 0, 0, 0, 0);
+			console.log(date);
+			self.startDate = date;
+
+			update();
+		}
+
+		function update() {
+			updateTasks();
+			self.tasks.removeClass("expectedComplete");
+			computeTimeDiff();
+			findIndex();
+		}
+		function writeDate(date, id) {
+			var year = date.getFullYear();
+			var month = date.getMonth() + 1;
+			var day = date.getDate();
+			var hours = date.getHours();
+
+			var s = id + ": " + month + " / " + day + " / " + year + ", " + hours + " : 00";
+			$("#" + id).empty().append(s);
+		}
+
+		function loadStartDate() {
+			var START_DATE_ID = "start_date";
+			writeDate(self.startDate, START_DATE_ID);
+		}
+
+		function loadCurrDate() {
+			var CURR_DATE_ID = "curr_date";
+
+			writeDate(self.currDate, CURR_DATE_ID);
+		}
+
+		function loadMinutesSince() {
+			var msInMinute = 60 * 1000;
+			var minutesInHour = 60;
+			var timeDiff = self.currDate - self.startDate;
+			var minuteDiff = timeDiff / msInMinute;
+			// round to 2 decimals
+			minuteDiff = Math.round(minuteDiff * 100) / 100;
+			var hourDiff = minuteDiff / minutesInHour;
+			// round to 2 decimals
+			hourDiff = Math.round(hourDiff * 100) / 100;
+
+			var id = "time_diff";
+
+			var s = id + ": " + minuteDiff + " minutes or " + hourDiff + " hours";
+
+			$("#" + id).empty().append(s);
+		}
+
+		return {
+			construct: construct,
+			print: print,
+			addTask: addTask,
+			findIndex: findIndex,
+			updateTasks: updateTasks,
+			setDate: setDate,
+			update: update
+		}
+	})();
+	adt.construct(startDate, currDate);
+	/* for (var i = 0 ; i < 50; i++) {
+		var taskDuration = Math.floor(Math.random() * 30) + 15;
+		adt.addTask(taskDuration);
+	} */
+	//adt.print();
+	adt.updateTasks();
+	adt.findIndex();
+
+	$("#submitdate").click(function(e) {
+		e.preventDefault();
+		var date = $("#datepicker").val();
+		adt.setDate(date);
+		hideDatepickerTimer = setTimeout(function() {
+			$("#startdate").slideUp(700);
+		}, 1000);
+	});
+
+	$(".task").bind("mousedown", function(e) {
+		e.preventDefault();
+		var mousedownType = e.which;
+		var RIGHT_CLICK = 3;
+
+		if (mousedownType === RIGHT_CLICK) {
+			$(this).remove();
+			adt.update();
+		}
+	});
+	$(".task").bind("contextmenu", function() {
+		return false;
+	});
+
+	var ENTRY_FORM_ID = "entry_form";
+	var ENTRY_SUBMIT_ID = "entry_submit";
+
+	var ADD_TASK_BUTTON_ID = "add_task";
+
+	var TASK_LIST_ID = "tasks";
+
+	$("#" + ADD_TASK_BUTTON_ID).click(function() {
+		showEntryBox();
+	});
+
+	$("#" + ENTRY_SUBMIT_ID).click(function(e) {
+		e.preventDefault();
+
+		var entry_title_selector = "#" + ENTRY_FORM_ID + " [name=entry_title]";
+		var entry_duration_selector = "#" + ENTRY_FORM_ID + " [name=entry_duration]";
+		var entry_color_selector = "#" + ENTRY_FORM_ID + " select";
+
+		var entryTitle = $(entry_title_selector).val();
+		var entryDuration = $(entry_duration_selector).val();
+		var entryColor = $(entry_color_selector).val();
+
+		addTask(entryTitle, entryDuration, entryColor);
+		newTaskTimer = setTimeout(function() {
+			$("#" + ENTRY_FORM_ID).slideUp(700);
+		}, 700);
+	});
+
+	function showEntryBox() {
+		$("#" + ENTRY_FORM_ID).show();
+		clearTimeout(newTaskTimer);
+	}
+
+	function addTask(title, duration, color) {
+		if (!title || title.length === 0) {
+			alert("Did Not Supply TITLE");
+			return;
+		}
+		if (!duration || duration.length === 0) {
+			alert("Did Not supply DURATION");
+			return;
+		}
+
+		var node = "<div class=\"task\">" + title + "<span class=\"duration\">" + duration +"</span><button class=\"completedTaskButton\">mark as Completed</button></div>";
+		node = $(node);
+		node.addClass(color);
+		node.addClass("notcomplete");
+		$("#" + TASK_LIST_ID).append(node);
+	}
+
 
 	$("button.completedTaskButton").click(function() {
 		$(this).parent().removeClass("notcomplete");
